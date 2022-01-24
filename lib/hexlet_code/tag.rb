dc68d791c911a9102caf7c4cbe729b3474dd92c5
minada <minada93@gmail.com>
2022-01-24 16:45:02 +0400

    add tests for build and refactor

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -3,35 +3,33 @@
 module HexletCode
   class Tag
     UNPAIRED_TAGS = %w[meta img link br hr input area param col base].freeze
+    class << self
+      def build(tag, **kvargs, &block)
+        result = []
 
-    def build(tag, **kvargs, &block)
-      result = []
-      if UNPAIRED_TAGS.include? tag
         result << "<"
         result << tag
-        put_the_hash(result, kvargs) if kvargs.any?
+        add_hash(result, kvargs) if kvargs.any?
         result << ">"
-      else
-        result << "<"
-        result << tag
-        put_the_hash(result, kvargs) if kvargs.any?
-        result << ">"
-        put_the_block(result, block) if block_given?
-        result << "</#{tag}>"
+
+        unless UNPAIRED_TAGS.include? tag
+          add_block(result, block) if block_given?
+          result << "</#{tag}>"
+        end
+        result.join
       end
-      result.join
-    end
 
-    private
+      private
 
-    def put_the_hash(result, kvargs)
-      kvargs.each do |k, v|
-        result << " #{k}=\"#{v}\""
+      def add_hash(result, kvargs)
+        kvargs.each do |k, v|
+          result << " #{k}=\"#{v}\""
+        end
       end
-    end
 
-    def put_the_block(result, block)
-      result << block.call.to_s
+      def add_block(result, block)
+        result << block.call.to_s
+      end
     end
   end
 end
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe HexletCode do
   it "has a version number" do
     expect(HexletCode::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
