3b81daf069b56b9a28468f954a30bcbe09bf9d44
minada <minada93@gmail.com>
2022-01-26 22:33:36 +0400

    task 4 add empty form

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,3 +2,15 @@
 
 require_relative "hexlet_code/version"
 require_relative "hexlet_code/tag"
+
+module HexletCode
+  class << self
+    def form_for(person, **kvargs, &block)
+      kvargs[:action] = kvargs.delete(:url) || "#"
+      kvargs[:method] = "post"
+      HexletCode::Tag.build("form", kvargs) do
+        "\n"
+      end
+    end
+  end
+end
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe HexletCode do
   it "has a version number" do
     expect(HexletCode::VERSION).not_to be nil
   end
+
+  describe ".form_for" do
+    User = Struct.new(:name, :job, keyword_init: true)
+    user = User.new name: "rob"
+
+    context "with user" do
+      it "return standart form tag" do
+        expect(HexletCode.form_for(user)).to eq("<form action=\"#\" method=\"post\">\n</form>")
+      end
+    end
+
+    context "with user and url" do
+      it "return form tag with url" do
+        expect(HexletCode.form_for(user, url: "/users")).to eq("<form action=\"/users\" method=\"post\">\n</form>")
+      end
+    end
+  end
 end
